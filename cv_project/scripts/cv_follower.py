bd781c21487a17bca2680532e0c8a20b961b0254
Cypress Frankenfeld <iamcypress@gmail.com>
2014-10-21 16:17:08 -0400

    Basic framework for a state machine. For now, make the robot stand still.
    How it works:
    assign controller.move to be any function that returns a Twist object.
    every timestep, the controller will call controller.move() and send that
    Twist object to the robot.
    
    If you want to exit the script gracefully while the robot is moving,
    the robot should stop automatically (we send a zero-vector Twist object
    when there's a signal interrupt)

--- cv_project/scripts/cv_follower.py
+++ cv_project/scripts/cv_follower.py
@@ -1,5 +1,39 @@
 #!/usr/bin/env python
 import rospy
+import signal
+import sys
+from geometry_msgs.msg import Twist
+
+
+class Controller:
+    def __init__(self):
+        rospy.init_node('controller', anonymous=True)
+        self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+        self.sub = None
+        self.move = self.do_nothing
+        self.running = False
+        signal.signal(signal.SIGINT, self.signal_handler)
+
+    def do_nothing(self):
+        return Twist()
+
+    def signal_handler(self, signal, frame):
+        self.running = False
+        self.pub.publish(Twist())
+        sys.exit(0)
+
+    def run(self):
+        self.running = True
+        rate = rospy.Rate(20)
+        while not rospy.is_shutdown() and self.running:
+            movement = self.move()
+            rospy.loginfo(movement)
+            self.pub.publish(movement)
+            rate.sleep()
 
 if __name__ == "__main__":
-    print("Hello world!")
+    try:
+        controller = Controller()
+        controller.run()
+    except rospy.ROSInterruptException:
+        pass
