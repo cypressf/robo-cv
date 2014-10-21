7f5d042aeb7003626caff22a456a57e75d23c5ba
Cypress Frankenfeld <iamcypress@gmail.com>
2014-10-21 16:46:28 -0400

    we probably don't need multiple states (removed that). added image subscriber

--- cv_project/scripts/cv_follower.py
+++ cv_project/scripts/cv_follower.py
@@ -3,6 +3,8 @@ import rospy
 import signal
 import sys
 from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Image
+import cv_bridge
 
 
 class Controller:
@@ -10,11 +12,13 @@ class Controller:
         rospy.init_node('controller', anonymous=True)
         self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
         self.sub = None
-        self.move = self.do_nothing
         self.running = False
         signal.signal(signal.SIGINT, self.signal_handler)
 
-    def do_nothing(self):
+    def image_received(self, img):
+        rospy.loginfo("Image received! %s" % str(img))
+
+    def move(self):
         return Twist()
 
     def signal_handler(self, signal, frame):
@@ -24,10 +28,10 @@ class Controller:
 
     def run(self):
         self.running = True
+        self.sub = rospy.Subscriber('camera/image_raw', Image, self.image_received)
         rate = rospy.Rate(20)
         while not rospy.is_shutdown() and self.running:
             movement = self.move()
-            rospy.loginfo(movement)
             self.pub.publish(movement)
             rate.sleep()
 
