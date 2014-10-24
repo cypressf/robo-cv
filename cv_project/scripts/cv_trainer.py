8627a36dd57cad44c6929281c773e31f2e2f12e3
Cypress Frankenfeld <iamcypress@gmail.com>
2014-10-24 15:25:19 -0400

    you can pass it a path to a bag file

--- cv_project/scripts/cv_trainer.py
+++ cv_project/scripts/cv_trainer.py
@@ -5,15 +5,19 @@ from geometry_msgs.msg import Twist
 from image_processing import extract_data
 import cv2
 import rosbag
+import pickle
+import sys
+
+SAVE_FILE_NAME = "saved_fit.txt"
 
 
 def twist_to_nparray(msg):
     return np.array([msg.linear.x, msg.angular.z])
 
 
-if __name__ == "__main__":
+def find_best_fit(bagfile_path):
     most_recent_cmd_vel = Twist()
-    bag = rosbag.Bag('/home/cypressf/robo-cv/rosbag/2014-10-21-16-07-47.bag')
+    bag = rosbag.Bag(bagfile_path)
     clf = Ridge(alpha=1.0)  # TODO: auto-calibrate alpha (it's easy using a scikit-learn one-liner)
     image_data = []
     cmd_vel_data = []
@@ -26,7 +30,15 @@ if __name__ == "__main__":
             image_data.append(extract_data(cv_image))
             cmd_vel_data.append(twist_to_nparray(most_recent_cmd_vel))
 
-    image_data_array = np.array(image_data)
-    cmd_vel_array = np.array(cmd_vel_data)
+    clf.fit(image_data, cmd_vel_data)
+    return clf
 
-    clf.fit(image_data,cmd_vel_data)
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Must give me the path to your bag file")
+    else:
+        bagfile_path = sys.argv[1]
+        clf = find_best_fit(bagfile_path)
+
+        with open(SAVE_FILE_NAME, 'w') as f:
+            pickle.dump(clf, f)
