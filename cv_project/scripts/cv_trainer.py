e83f934d336c20f1036f10a17b30bc8e6e8a4373
Cypress Frankenfeld <iamcypress@gmail.com>
2014-10-29 11:49:56 -0400

    now look at multiple bag files

--- cv_project/scripts/cv_trainer.py
+++ cv_project/scripts/cv_trainer.py
@@ -7,6 +7,7 @@ import cv2
 import rosbag
 import pickle
 import sys
+import os
 
 SAVE_FILE_NAME = "saved_fit.txt"
 
@@ -15,30 +16,32 @@ def twist_to_nparray(msg):
     return np.array([msg.linear.x, msg.angular.z])
 
 
-def find_best_fit(bagfile_path):
-    most_recent_cmd_vel = Twist()
-    bag = rosbag.Bag(bagfile_path)
+def find_best_fit(bagfiles):
     clf = Ridge(alpha=1.0)  # TODO: auto-calibrate alpha (it's easy using a scikit-learn one-liner)
     image_data = []
     cmd_vel_data = []
-    for topic, msg, t in bag.read_messages(topics=['/camera/image_raw/compressed', '/cmd_vel'], ):
-        if topic == "/cmd_vel":
-            most_recent_cmd_vel = msg
-        elif topic == "/camera/image_raw/compressed":
-            np_arr = np.fromstring(msg.data, np.uint8)
-            cv_image = cv2.imdecode(np_arr, cv2.CV_LOAD_IMAGE_COLOR)
-            image_data.append(extract_data(cv_image))
-            cmd_vel_data.append(twist_to_nparray(most_recent_cmd_vel))
+    for bagfile_path in bagfiles:
+        most_recent_cmd_vel = None
+        bag = rosbag.Bag(bagfile_path)
+        for topic, msg, t in bag.read_messages(topics=['/camera/image_raw/compressed', '/cmd_vel'], ):
+            if topic == "/cmd_vel":
+                most_recent_cmd_vel = msg
+            elif topic == "/camera/image_raw/compressed" and most_recent_cmd_vel is not None:
+                np_arr = np.fromstring(msg.data, np.uint8)
+                cv_image = cv2.imdecode(np_arr, cv2.CV_LOAD_IMAGE_COLOR)
+                image_data.append(extract_data(cv_image))
+                cmd_vel_data.append(twist_to_nparray(most_recent_cmd_vel))
 
     clf.fit(image_data, cmd_vel_data)
     return clf
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("Must give me the path to your bag file")
+        print("Must give me the path to your bagfile directory")
     else:
-        bagfile_path = sys.argv[1]
-        clf = find_best_fit(bagfile_path)
+        bagfile_directory = sys.argv[1]
+        bagfiles = [os.path.join(bagfile_directory, file) for file in os.listdir(bagfile_directory) if file.endswith(".bag")]
+        clf = find_best_fit(bagfiles)
 
         with open(SAVE_FILE_NAME, 'w') as f:
             pickle.dump(clf, f)
