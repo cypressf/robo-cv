3f15f1b71a178bad49a4aa62e9cbb40b653b7306
Halie Murray-Davis <hmurraydavis@yahoo.com>
2014-11-04 15:28:47 -0500

    added coments, refactored into comments, blue cup finding function

--- cv_project/scripts/image_processing.py
+++ cv_project/scripts/image_processing.py
@@ -3,6 +3,12 @@ import cv2
 import numpy as np
 
 def closeImages():
+    '''Close images opened by Open CV, addresses the different method of closing
+    images based on command line or called usage.
+    
+    INPUT: none
+    OUTPUT: none
+    **Closes all open cv2 images either on a keystroke (comman line use) or immediately (called)'''
     if __name__=='__main__':
         cv2.waitKey(0)
         cv2.destroyAllWindows()
@@ -42,6 +48,37 @@ def hsv_test(cv_image):
     cv2.waitKey(3)
     return np.array(np.sum(v, axis=0))
 
+def colorImgPreProcess(image):
+    #do processing on the image while it's still in color
+    image=cv2.medianBlur(image,7) #kernal size must be odd
+    image=cv2.bilateralFilter(image,9,75,75)
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV) #convert to hsv image
+    for i in range(hsv_image.shape[0]):
+        print hsv_image[i,:,0]
+
+    cv2.imshow('img',image)
+    closeImages()
+    return hsv_image
+    
+def imageProcessBlueCups(image):
+    '''Process an input image with Open CV methods to focus on blue cup like shapes
+    
+    INPUT: image
+    OUTPUT: binary image better suited for prcessing with the ridge regression'''
+    
+    #pre-process image while it's in color
+    colorImgInput=colorImgPreProcess(image) 
+    
+    # define range of blue color for HSV
+    lower_blue = np.array([170,30,50])
+    upper_blue = np.array([270,360,255])
+
+    # Threshold the HSV image to get only blue colors
+    mask = cv2.inRange(colorImgInput, lower_blue, upper_blue)
+    
+    cv2.imshow('blue cup mask',mask)
+    closeImages()
+    
 
 def imageProcessRedCups(image):
     '''Process an input image with Open CV methods to focus on red cup like shapes
@@ -49,35 +86,47 @@ def imageProcessRedCups(image):
     INPUT: image
     OUTPUT: binary image better suited for prcessing with the ridge regression'''
     
-    #do processing on the image while it's still in color
-    image=cv2.medianBlur(image,9) #kernal size must be odd
-    image=cv2.bilateralFilter(image,9,75,75)
-    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV) #convert to hsv image
-
-    # define range of blue color in HSV
-    lower_red = np.array([0,50,50])
-    upper_red = np.array([50,255,255])
+    #pre-process image while it's in color
+    colorImgInput=colorImgPreProcess(image) 
+    
+    # define range of red color for HSV
+    lower_red1 = np.array([0,50,50])
+    upper_red1 = np.array([70,105,85])
+    lower_red2 = np.array([220,0,0])
+    upper_red2 = np.array([255,255,255])
 
     # Threshold the HSV image to get only red colors
-    mask = cv2.inRange(hsv_image, lower_red, upper_red)
+    mask1 = cv2.inRange(colorImgInput, lower_red1, upper_red1)
+    mask2 = cv2.inRange(colorImgInput, lower_red2, upper_red2)
+    mask=cv2.add(mask1,mask2)
 
     #dialate image to filter it:
     kernel = np.ones((6,6),np.uint8) #make 5 by 5 kernal size filter
-    dilated = cv2.dilate(mask,kernel,iterations = 1)
+    dilated = cv2.dilate(mask1,kernel,iterations = 1)
 
+    #Contor stuff, in dev
     contours,hierarchy = cv2.findContours(dilated, 1, 2)
     cnt = contours[0]
     M = cv2.moments(cnt)
-    #print M
+    print M
+    
+    cv2.drawContours(mask, contours, -1, (40,255,0), 3)
 
-    #cv2.imshow('img',image)
+    cv2.imshow('mask 1',mask1)
+    cv2.imshow('mask 2',mask2)
+    cv2.imshow('mask',mask)
+#    cv2.imshow('drawn contors', contors)
     #cv2.imshow('dilated image', dilated)
 
-    cv2.imshow('mask',mask)
+    
+    
     closeImages()
 
     
     
 if __name__=='__main__':
-    image = cv2.imread('blurrySoloCups.jpg',cv2.IMREAD_COLOR)
+    image = cv2.imread('cups.jpg',cv2.IMREAD_COLOR)
+    print image.shape
+    closeImages()
     imageProcessRedCups(image)
+#    imageProcessBlueCups(image)
