90a1f10bc78aa687699fece487ab1ef4372b2a4a
Halie Murray-Davis <hmurraydavis@yahoo.com>
2014-11-03 22:05:40 -0500

    added image processing code to function

--- cv_project/scripts/image_processing.py
+++ cv_project/scripts/image_processing.py
@@ -35,3 +35,39 @@ def hsv_test(cv_image):
     cv2.imshow("image", mask_results)
     cv2.waitKey(3)
     return np.array(np.sum(v, axis=0))
+    
+def imageProcessRedCups(imageName):
+    '''Process an input image with Open CV methods to focus on red cup like shapes'''
+    image=imageName
+    image = cv2.imread(image,cv2.IMREAD_COLOR)
+    image=cv2.medianBlur(image,9) #kernal size must be odd
+    image=cv2.bilateralFilter(image,9,75,75)
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV) #convert to hsv image
+
+    # define range of blue color in HSV
+    lower_blue = np.array([0,50,50])
+    upper_blue = np.array([50,255,255])
+
+    # Threshold the HSV image to get only blue colors
+    mask = cv2.inRange(hsv_image, lower_blue, upper_blue)
+
+    #erode image to filter it:
+    kernel = np.ones((6,6),np.uint8) #make 5 by 5 kernal size filter
+    dilated = cv2.dilate(mask,kernel,iterations = 1)
+
+    contours,hierarchy = cv2.findContours(dilated, 1, 2)
+
+    cnt = contours[0]
+    M = cv2.moments(cnt)
+    print M
+
+    cv2.imshow('img',image)
+    #cv2.imshow('dilated image', dilated)
+
+    cv2.imshow('mask',mask)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
+    
+if __name__=='__main__':
+    imageProcessRedCups('blurrySoloCups.jpg')
